e8b35a1efe59db6d71e13618389ab900f245dba9
curtisgeorge <cgeorge7@san.rr.com>
2016-07-28 09:48:39 -0700

    Added script for creating cross compiler tools

---
+++ scripts/cross-compiler.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+export PREFIX="$HOME/usr/local"
+export TARGET=arm-none-eabi
+export PATH="$PREFIX/bin:$PATH"
+
+tar xzvf binutils-2.25.tar.gz
+cd binutils-2.25
+mkdir build
+cd build
+../configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make
+make install
+
+cd ../..
+tar xzvf gcc-5.3.0.tar.gz
+cd gcc-5.3.0
+mkdir build
+cd build
+../configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+#make all-gcc
+#make all-target-libgcc
+#make install-gcc
+#make install-target-libgcc
+make all-host
+make install-host
