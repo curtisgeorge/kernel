e8e7ed5c9a7ccd61871406a952886c3fcac34dca
curtisgeorge <cgeorge7@san.rr.com>
2016-07-30 21:00:56 -0700

    Minor updates to color lcd code.

--- arch/arm/clcd.c
+++ arch/arm/clcd.c
@@ -1,18 +1,26 @@
 #include "clcd.h"
 
-unsigned int* fb_ptr = (unsigned int*) 0x100000;
+static const unsigned int H_SIZE = 320;
+static const unsigned int V_SIZE = 240;
+static const unsigned int CLCD_TIME0_REG_ADDR = 0x10120000;
+static const unsigned int CLCD_TIME1_REG_ADDR = 0x10120004;
+static const unsigned int CLCD_TIME2_REG_ADDR = 0x10120008;
 
-void set_pixel(unsigned char x, unsigned char y, unsigned int val);
+static const unsigned int CLCD_VGA_TIME0_VAL = 0x9F7FBF4C;
+static const unsigned int CLCD_VGA_TIME1_VAL = 0x818360EF;
+static const unsigned int CLCD_VGA_TIME2_VAL = 0x053F1800;
+
+static unsigned int* fb_ptr = (unsigned int*) 0x100000;
 
 void init_clcd() {
   *(volatile unsigned int *)(0x1000001C) = 0x2C77; /* VGA Timing */
-  *(volatile unsigned int *)(0x10120000) = 0x9F7FBF4C;
-  *(volatile unsigned int *)(0x10120004) = 0x818360eF;
-  *(volatile unsigned int *)(0x10120008) = 0x053F1800;
+  *(volatile unsigned int *)(CLCD_TIME0_REG_ADDR) = CLCD_VGA_TIME0_VAL;
+  *(volatile unsigned int *)(CLCD_TIME1_REG_ADDR) = CLCD_VGA_TIME1_VAL;
+  *(volatile unsigned int *)(CLCD_TIME2_REG_ADDR) = CLCD_VGA_TIME2_VAL;
   *(volatile unsigned int *)(0x10120010) = 0x100000; /* base addr of frame buffer */
-  *(volatile unsigned int *)(0x10120018) = 0x82b; /* control bits */
+  *(volatile unsigned int *)(0x10120018) = 0x82B; /* control bits */
 }
 
 void set_pixel(unsigned char x, unsigned char y, unsigned int val) {
-  fb_ptr[x + (320 * y)] = val;
+  fb_ptr[x + (H_SIZE * y)] = val;
 }
