2e7971afaef2ed56d8f6626b3e775e329cd75acf
curtisgeorge <cgeorge7@san.rr.com>
2016-08-10 11:46:33 -0700

    Minor change to cr2 header.

--- kernel/arch/x86/include/asm/cr2.h
+++ kernel/arch/x86/include/asm/cr2.h
@@ -5,11 +5,11 @@
 /*
  * CR2 is an x86 control register.  It holds the faulting address
  * if a pauge fault occurs.  This provides and inline assembly
- * method to get the value out of cr2.
+ * method to get the value out of CR2.
  */
 
 static inline void get_cr2() {
   uint32_t cr2;
-  asm volatile ("mov %%cr2 %0" : "=r"(cr2));
+  asm volatile ("movl %%cr2 %0" : "=r"(cr2));
   return cr2;
 }
